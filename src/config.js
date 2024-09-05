6d88f19f8bdfa8bb71dd174fb706dd895cd7c6d0
IvanOliveiraaa <ivosn9@gmail.com>
2024-09-05 05:22:47 -0400

    variavel global

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default class Config {
   static backend2 = "https://back-ponto-c8dm-git-master-ivanoliveiraa.vercel.app";
-  static backend = "https://back-ponto-production.up.railway.app";
+  static backend = process.env.BACKEND_URL;
 
 }
